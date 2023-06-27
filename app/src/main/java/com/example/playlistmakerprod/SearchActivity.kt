863c6d3572742f6b9e184e7d8470d1899bffc605
YBarinov <foodcats@ya.ru>
2023-06-27 22:06:20 +0300

    Работа над спринтом №9 (ДЗ 4)

--- app/src/main/java/com/example/playlistmakerprod/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmakerprod/SearchActivity.kt
@@ -1,11 +1,66 @@
 package com.example.playlistmakerprod
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+    var userText: String? = null
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("USER_SAVED_INPUT",userText)
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val inputEditText = findViewById<EditText>(R.id.search_edit_text)
+        inputEditText.setText(savedInstanceState.getString("USER_SAVED_INPUT",""))
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val backBtn = findViewById<ImageView>(R.id.search_back_button)
+        backBtn.setOnClickListener {
+            this.finish()
+        }
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        val inputEditText = findViewById<EditText>(R.id.search_edit_text)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+            inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+            inputEditText.clearFocus()
+        }
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+                userText = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
     }
+
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+
 }
