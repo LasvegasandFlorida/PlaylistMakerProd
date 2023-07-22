544b20fbf60fc2b18bcf32d1f99cb1b6fc256ea6
YBarinov <foodcats@ya.ru>
2023-07-22 19:03:51 +0300

    - Добавлен RecyclerView для отображения треков - RecyclerView заполнен мок-объектами

--- app/src/main/java/com/example/playlistmakerprod/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmakerprod/SearchActivity.kt
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class SearchActivity : AppCompatActivity() {
     var userText: String? = null
+    var listOfSongs= emptyList<Track>()
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString("USER_SAVED_INPUT",userText)
@@ -28,10 +35,24 @@ class SearchActivity : AppCompatActivity() {
         backBtn.setOnClickListener {
             this.finish()
         }
+        listOfSongs= listOf<Track>(
+            Track(getString(R.string.smells_like_teen_spirit_name),getString(R.string.smells_like_teen_spirit_artist),getString(
+                R.string.smells_like_teen_spirit_length),getString(R.string.smells_like_teen_spirit_image)),
+            Track(getString(R.string.billie_jean_name),getString(R.string.billie_jean_artist),getString(
+                R.string.billie_jean_length),getString(R.string.billie_jean_image)),
+            Track(getString(R.string.stayin_alive_name),getString(R.string.stayin_alive_artist),getString(
+                R.string.stayin_alive_length),getString(R.string.stayin_alive_image)),
+            Track(getString(R.string.whole_lotta_love_name),getString(R.string.whole_lotta_love_artist),getString(
+                R.string.whole_lotta_love_length),getString(R.string.whole_lotta_love_image)),
+            Track(getString(R.string.sweet_child_o_mine_name),getString(R.string.sweet_child_o_mine_artist),getString(
+                R.string.sweet_child_o_mine_length),getString(R.string.sweet_child_o_mine_image))
+        )
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val songsAdapter =SongsAdapter(listOfSongs)
+        recyclerView.adapter = songsAdapter
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
         val inputEditText = findViewById<EditText>(R.id.search_edit_text)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
-
         clearButton.setOnClickListener {
             onClearClick(inputEditText,inputMethodManager)
         }
@@ -66,5 +87,38 @@ class SearchActivity : AppCompatActivity() {
         }
     }
 
+
+}
+class SongsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    private val songImage: ImageView = itemView.findViewById(R.id.songImage)
+    private val songName: TextView = itemView.findViewById(R.id.songName)
+    private val songAuthor: TextView = itemView.findViewById(R.id.songAuthor)
+    private val songLength: TextView = itemView.findViewById(R.id.songLength)
+
+    fun bind(model: Track) {
+        songName.text = model.trackName
+        songAuthor.text = model.artistName
+        songLength.text = model.trackTime
+        Glide.with(itemView).load(model.artworkUrl100).centerCrop().transform(RoundedCorners(2)).placeholder(R.drawable.placeholder).into(songImage)
+    }
+
+}
+class SongsAdapter(
+    private val tracks: List<Track>
+) : RecyclerView.Adapter<SongsViewHolder> () {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongsViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.songs_view, parent, false)
+        return SongsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SongsViewHolder, position: Int) {
+        holder.bind(tracks[position])
+    }
+
+    override fun getItemCount(): Int {
+        return tracks.size
+    }
 
 }
